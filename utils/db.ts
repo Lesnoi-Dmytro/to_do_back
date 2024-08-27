30e8ed06de86bf703d1d4b56d23b36967b05f06d
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2024-08-27 14:33:30 +0300

    Updated Mongo connection

--- utils/db.ts
+++ utils/db.ts
@@ -1,31 +1,15 @@
-// import mongoose = require('mongoose');
-//
-// const uri = "mongodb+srv://dmytrolesnoi:vIIk0JdPyydR4wFi@mongodb.n8ct3nx.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB";
-//
-// const connectToDatabase = async () => {
-//     try {
-//         await mongoose.connect(uri);
-//         console.log('Mongoose connected successfully');
-//     } catch (error) {
-//         console.error('Mongoose connection error: ' + error);
-//         process.exit(1);
-//     }
-// };
-//
-// export default connectToDatabase;
+import mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://dmytrolesnoi:<db_password>@mongodb.n8ct3nx.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
+const uri = "mongodb+srv://mongoose_user:fxmRMFnUsLv1hdvT@mongodb.n8ct3nx.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB";
+
+const connectToDatabase = async () => {
     try {
-        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-        await mongoose.connect(uri, clientOptions);
-        await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await mongoose.disconnect();
+        await mongoose.connect(uri);
+        console.log('Mongoose connected successfully');
+    } catch (error) {
+        console.error('Mongoose connection error: ' + error);
+        process.exit(1);
     }
-}
-run().catch(console.dir);
+};
+
+export default connectToDatabase;
